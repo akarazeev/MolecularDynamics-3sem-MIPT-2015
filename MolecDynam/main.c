42175e6daa5e73501b1e8cb949933bddf21398ce
Akarazeev <thawetret@gmail.com>
2015-09-17 00:56:12 +0300

    Initial Commit

---
+++ MolecDynam/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MolecDynam
+//
+//  Created by Anton Wetret on 17/09/15.
+//  Copyright (c) 2015 Anton Karazeev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
