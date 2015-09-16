02ccedec926d005967de989559f8b81923552515
Akarazeev <thawetret@gmail.com>
2015-09-17 01:43:35 +0300

    Add CalcEnergy()

--- MolecDynam/main.c
+++ MolecDynam/main.c
@@ -7,9 +7,12 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 const int N = 3;
 const double dt = 0.1;
+double U = 0;
 
 double v[N][3];
 double f[N][3];
@@ -46,25 +49,45 @@ void CalcForces() {
             double r1 = sqrt(r2);
 //            utot += u(r1); // Potential
 //            f_r = fr(r); // Force/Range
+            //TODO: Force/Range
+            double f_r = 1 / r1;
             for (int k = 0; k < 3; ++k) {
-//                f[i][k] += f_r * rij[k];
-//                f[j][k] -= f_r * rij[k];
+                f[i][k] += f_r * rij[k];
+                f[j][k] -= f_r * rij[k];
             }
         }
     }
 }
 
-int main() {
+//FIXME: Works wrong because of Potentials
+void CalcEnergy() {
+    U = 0;
     for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            printf("%f ", v[i][j]);
+        for (int k = 0; k < 3; ++k) {
+            U += m[i] * v[i][k] * v[i][k] / 2;
         }
-        printf("\n");
+    }
+}
+
+int main() {
+    m[0] = 1;
+    m[1] = 2;
+    m[2] = 0.7;
+    for (int i = 0; i < N; ++i) {
+        for (int k = 0; k < 3; ++k) {
+            r[i][k] = 1 + rand() % 10;
+        }
     }
     for (int i = 0; i < 100; ++i) {
         ClearForces();
         CalcForces();
         EqMotion();
+        CalcEnergy();
+//        for (int k = 0; k < 3; ++k) {
+//            printf("%f %f ", v[1][k], r[1][k]);
+//        }
+//        printf("\n");
+        printf("%f\n", U);
     }
     return 0;
 }
